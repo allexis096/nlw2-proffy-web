131a6df0eee2c77484356784a7e3ee1223ff6804
allexis096 <allexissoares@gmail.com>
2020-08-03 19:03:52 -0300

    global css edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import './assets/styles/global.css';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <button type="button">Clica em mim</button>
+    </div>
   );
 }
 
